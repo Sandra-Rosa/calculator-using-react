8a9965f5a06106cb0675448d8a9434b5aa9ec025
Sandra-Rosa <sandraantony2002@gmail.com>
2022-08-10 11:35:39 +0530

    Designed a general layout

--- src/App.js
+++ src/App.js
@@ -1,42 +1,18 @@
 import './App.css';
-
+import Logo from "./girl.png";
 function App() {
   return (
-    <div className="Calculator1">
-    <div className="calculator">
-    <input type="text" className="calculator-screen" value="0" disabled />
-
-  <div class="calculator-keys">
-
-    <button type="button" className="operator" value="+">+</button>
-    <button type="button" className="operator" value="-">-</button>
-    <button type="button" className="operator" value="*">&times;</button>
-    <button type="button" className="operator" value="/">&divide;</button>
-
-    <button type="button" value="7">7</button>
-    <button type="button" value="8">8</button>
-    <button type="button" value="9">9</button>
-
-
-    <button type="button" value="4">4</button>
-    <button type="button" value="5">5</button>
-    <button type="button" value="6">6</button>
-
-
-    <button type="button" value="1">1</button>
-    <button type="button" value="2">2</button>
-    <button type="button" value="3">3</button>
-
-
-    <button type="button" value="0">0</button>
-    <button type="button" className="decimal" value=".">.</button>
-    <button type="button" className="all-clear" value="all-clear">AC</button>
-
-    <button type="button" className="equal-sign" value="=">=</button>
-
-  </div>
-</div>
-</div>
+    <div className='Calculator'>
+      <span className="nav">Sandra Rosa Antony</span>
+      <div className="main">
+      <img src= {Logo} className='girl' alt="logo" />
+      <div className="content">
+      <span className="Heading">
+            Calculator
+          </span>
+      </div>
+      </div>
+    </div>
   );
 }
 
