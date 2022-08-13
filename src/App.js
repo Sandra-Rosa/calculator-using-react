6d63844a3665822e911f06a6b4fccdfb084eedaf
Sandra-Rosa <sandraantony2002@gmail.com>
2022-08-13 10:24:35 +0530

    all colour combinations and responsize design is finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import './App.css';
 import Logo from "./girl.png";
 function App() {
+  const createDigits=()=>{
+    const digits=[];
+    for(let i=1;i<10;i++){
+      digits.push(
+        <button key={i}>{i}</button>
+      )
+    }
+    return digits;
+  }
   return (
-    <div className='Calculator'>
+    <div className='Calculator1'>
       <span className="nav">Sandra Rosa Antony</span>
       <div className="main">
-      <img src= {Logo} className='girl' alt="logo" />
-      <div className="content">
-      <span className="Heading">
+        <img src={Logo} className='girl' alt="logo" />
+        <div className="content">
+          <span className="Heading">
             Calculator
           </span>
-      </div>
+        
+        <div className="calculator">
+          <div className="display">
+            <span>(0)</span>0
+          </div>
+          <div className="operators">
+            <button>/</button>
+            <button>*</button>
+            <button>+</button>
+            <button>-</button>
+
+            <button>DEL</button>
+          </div>
+          <div className="digits">
+            {createDigits()}
+            <button>0</button>
+            <button>.</button>
+            <button>=</button>
+          </div>
+        </div>
+        </div>
       </div>
     </div>
   );
